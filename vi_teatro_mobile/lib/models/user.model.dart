9e537fbeeba21390866f210e9008a9001722cffd
Ewerton Luis <ewerton.santana@ucsal.edu.br>
2020-09-14 09:38:36 -0300

    Adicionada tela de cadastro de usuario

--- vi_teatro_mobile/lib/models/user.model.dart
+++ vi_teatro_mobile/lib/models/user.model.dart
@@ -7,11 +7,17 @@ class UserModel {
     this.id,
     this.firebaseCode,
     this.type,
+    this.email,
+    this.name,
+    this.password,
   });
 
   final int id;
-  final String firebaseCode;
-  final UserEnum type;
+  String firebaseCode;
+  UserEnum type;
+  String email;
+  String name;
+  String password;
 
   static List<UserModel> convertJsonList(dynamic values) {
     var list = List<UserModel>();
@@ -21,6 +27,9 @@ class UserModel {
         id: item['id'],
         firebaseCode: item['firebaseCode'],
         type: EnumToString.fromString(UserEnum.values, item['type']),
+        email: item['email'],
+        name: item['name'],
+        password: item['password'],
       );
       list.add(userModel);
     }
@@ -30,5 +39,8 @@ class UserModel {
   Map<String, dynamic> toMap() => {
         'firebaseCode': this.firebaseCode,
         'type': EnumToString.parse(this.type),
+        'email': this.email,
+        'name': this.name,
+        'password': this.password,
       };
 }
--- vi_teatro_mobile/lib/screens/home/home.dart
+++ vi_teatro_mobile/lib/screens/home/home.dart
@@ -1,10 +1,14 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
 import 'package:vi_teatro_mobile/api/response.dart';
 import 'package:vi_teatro_mobile/language/demo_localizations.dart';
 import 'package:vi_teatro_mobile/models/user.model.dart';
 import 'package:vi_teatro_mobile/providers/user.provider.dart';
-import 'package:http/http.dart' as http;
+
+import '../../models/user.model.dart';
 
 class Home extends StatefulWidget {
   const Home({
@@ -12,6 +16,7 @@ class Home extends StatefulWidget {
   });
 
   final http.Client httpClient;
+
   @override
   _HomeState createState() => _HomeState();
 }
@@ -44,46 +49,132 @@ class _HomeState extends State<Home> {
     };
   }
 
+  final _formKey = GlobalKey<FormState>();
+  final scaffoldKey = new GlobalKey<ScaffoldState>();
+  final user = UserModel();
+  bool isManager = false;
+
   @override
   Widget build(BuildContext context) {
-    return Consumer<UserProvider>(builder: (context, provider, child) {
-      dynamic status = getValuesStatus(provider.statusFetching);
-      return Scaffold(
-        appBar: AppBar(
-          centerTitle: false,
-          title: Text(
-            DemoLocalizations.of(context).appName,
+    return Consumer<UserProvider>(
+      builder: (context, provider, child) {
+        dynamic status = getValuesStatus(provider.statusFetching);
+        return Scaffold(
+          key: scaffoldKey,
+          appBar: AppBar(
+            centerTitle: false,
+            title: Text(
+              DemoLocalizations.of(context).appName,
+            ),
           ),
-        ),
-        body: Column(
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                FloatingActionButton(
-                  child: Icon(Icons.add),
-                  onPressed: () => provider.addUser(
-                    clientHttp: widget.httpClient,
-                    user: UserModel(
-                      firebaseCode: 'teste',
-                      type: UserEnum.MANAGER,
-                    ),
+          body: Column(
+            children: <Widget>[
+              Form(
+                key: _formKey,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 24.0, right: 24.0),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 24),
+                        child: Text(
+                          "VI Theater",
+                          style: TextStyle(
+                            fontSize: 36,
+                            fontWeight: FontWeight.w100,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        "Teatrão mo filho",
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 26, bottom: 24),
+                        child: Text(
+                          "Cadastrar-se",
+                          style: TextStyle(
+                            fontSize: 32,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(hintText: "Nome"),
+                        validator: (value) => value.isEmpty ? "O nome é obrigatório" : null,
+                        onSaved: (name) => user.name = name,
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(hintText: "Email"),
+                        validator: (value) => value.isEmpty ? "O email é obrigatório" : null,
+                        onSaved: (email) => user.email = email,
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(hintText: "Senha"),
+                        validator: (value) => value.isEmpty ? "O senha é obrigatório" : null,
+                        onSaved: (password) => user.password = password,
+                      ),
+                      Row(
+                        children: [
+                          Text("Cliente"),
+                          Switch(
+                            value: isManager,
+                            onChanged: (_) => {
+                              setState(() => isManager = !isManager),
+                            },
+                          ),
+                          Text("Gerente"),
+                        ],
+                      ),
+                      RaisedButton(
+                        onPressed: () => onSignUpClicked(provider),
+                        child: Text("Cadastrar"),
+                      ),
+                      Text("Ja tem uma conta?"),
+                      GestureDetector(
+                        onTap: () => print("tocou"),
+                        child: Text(
+                          "Acessar",
+                          style: TextStyle(
+                            decoration: TextDecoration.underline,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                Text('Teste adicionar 1 usuário'),
-              ],
-            ),
-            Row(
-              children: <Widget>[
-                Text('Status: '),
-                Text(
-                  status['text'],
-                  style: TextStyle(color: status['color']),
-                )
-              ],
-            )
-          ],
-        ),
-      );
-    });
+              ),
+              Row(
+                children: <Widget>[
+                  Text('Status: '),
+                  Text(
+                    status['text'],
+                    style: TextStyle(color: status['color']),
+                  )
+                ],
+              )
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  onSignUpClicked(UserProvider provider) {
+    return {
+      if (_formKey.currentState.validate())
+        {
+          _formKey.currentState.save(),
+          user.type = isManager ? UserEnum.MANAGER : UserEnum.CLIENT,
+          user.firebaseCode = Random.secure().nextDouble().toStringAsPrecision(10),
+          provider.addUser(
+            clientHttp: widget.httpClient,
+            user: user,
+          ),
+        },
+    };
   }
 }
